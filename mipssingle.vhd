c514596c4f0e653f5d832c35097ccd79bf34a37b
Carlos Eduardo Garcia <carlos.sena.garcia@usp.br>
2019-12-01 16:02:58 -0200

    fix2

--- mipssingle.vhd
+++ mipssingle.vhd
@@ -2,54 +2,6 @@
 -- From Section 7.6 of Digital Design & Computer Architecture
 -- Updated to VHDL 2008 26 July 2011 David_Harris@hmc.edu
 
-library IEEE; 
-use IEEE.STD_LOGIC_1164.all; use IEEE.NUMERIC_STD_UNSIGNED.all;
-use STD.TEXTIO.all;
-use IEEE.STD_LOGIC_ARITH.all;
-
-entity testbench is
-end;
-
-architecture test of testbench is
-  component top
-    port(clk, reset:           in  STD_LOGIC;
-         writedata, dataadr:   out STD_LOGIC_VECTOR(31 downto 0);
-         memwrite:             out STD_LOGIC);
-  end component;
-  signal writedata, dataadr:    STD_LOGIC_VECTOR(31 downto 0);
-  signal clk, reset,  memwrite: STD_LOGIC;
-begin
-
-  -- instantiate device to be tested
-  dut: top port map(clk, reset, writedata, dataadr, memwrite);
-
-  -- Generate clock with 10 ns period
-  process begin
-    clk <= '1';
-    wait for 5 ns;
-    clk <= '0';
-    wait for 5 ns;
-  end process;
-
-  -- Generate reset for first two clock cycles
-  process begin
-    reset <= '1';
-    wait for 22 ns;
-    reset <= '0';
-    wait;
-  end process;
-
-  -- check that 7 gets written to address 84 at end of program
-  process (clk) begin
-    if (clk'event and clk = '0' and memwrite = '1') then
-      if (to_integer(dataadr) = 84 and to_integer(writedata) = 7) then
-        report "NO ERRORS: Simulation succeeded" severity failure;
-      elsif (dataadr /= 80) then
-        report "Simulation failed" severity failure;
-      end if;
-    end if;
-  end process;
-end;
 
 ----------------------------------------------------------------------------------------------
 library IEEE; 
@@ -773,4 +725,56 @@ begin
                        writedata, readdata);
   imem1: imem port map(pc(7 downto 2), instr);
   dmem1: dmem port map(clk, memwrite, dataadr, writedata, readdata);
+end;
+
+----------------------------------------------------------------------------------------------
+
+
+library IEEE; 
+use IEEE.STD_LOGIC_1164.all; use IEEE.NUMERIC_STD_UNSIGNED.all;
+use STD.TEXTIO.all;
+use IEEE.STD_LOGIC_ARITH.all;
+
+entity testbench is
+end;
+
+architecture test of testbench is
+  component top
+    port(clk, reset:           in  STD_LOGIC;
+         writedata, dataadr:   out STD_LOGIC_VECTOR(31 downto 0);
+         memwrite:             out STD_LOGIC);
+  end component;
+  signal writedata, dataadr:    STD_LOGIC_VECTOR(31 downto 0);
+  signal clk, reset,  memwrite: STD_LOGIC;
+begin
+
+  -- instantiate device to be tested
+  dut: top port map(clk, reset, writedata, dataadr, memwrite);
+
+  -- Generate clock with 10 ns period
+  process begin
+    clk <= '1';
+    wait for 5 ns;
+    clk <= '0';
+    wait for 5 ns;
+  end process;
+
+  -- Generate reset for first two clock cycles
+  process begin
+    reset <= '1';
+    wait for 22 ns;
+    reset <= '0';
+    wait;
+  end process;
+
+  -- check that 7 gets written to address 84 at end of program
+  process (clk) begin
+    if (clk'event and clk = '0' and memwrite = '1') then
+      if (to_integer(dataadr) = 84 and to_integer(writedata) = 7) then
+        report "NO ERRORS: Simulation succeeded" severity failure;
+      elsif (dataadr /= 80) then
+        report "Simulation failed" severity failure;
+      end if;
+    end if;
+  end process;
 end;
